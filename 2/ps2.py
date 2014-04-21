2b4c5c9da65323f4960eb24cc9bf29a776f5916f
Bryance Oyang <boyang@caltech.edu>
2014-04-20 20:02:22 -0700

    working on ps2

--- 2/ps2.py
+++ 2/ps2.py
@@ -2,28 +2,31 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate
 from matplotlib.ticker import FormatStrFormatter
 
-def g(x):
-	return x*x*x*x*x
-
+# use trapezoid thing to integrate using N subdivisions
+# returns \int_a^b func(x)\,dx
 def trapezoidal(func, a, b, N):
 	[a, b, N] = map(float, [a, b, N])
 	h = (b - a)/N
 	x = np.arange(1, N) * h + a
 	return h * (sum(func(x)) + func(a)/2 + func(b)/2)
 
+# use simpson's thing to integrate using N subdivisions
+# returns \int_a^b func(x)\,dx
 def simpson(func, a, b, N):
 	[a, b, N] = map(float, [a, b, N])
 	h = (b - a)/N
 	key = np.arange(1, N) * h + a
 	mid = np.arange(0, N) * h + a + h/2
-	return h * (sum(func(key))/3 + 2*sum(func(mid))/3 \
-		+ func(a)/6 + func(b)/6)
+	return (h * (sum(func(key))/3 + 2*sum(func(mid))/3
+		+ func(a)/6 + func(b)/6))
 
+# make plots to see convergence of \int_0^1 \exp(x)\,dx as
+# number of subdivisions (N) increases for both trapezoid and simpson
 N = np.arange(1, 36)
 plt.clf()
-
 plt.subplot(2, 1, 1)
 trap_integral = np.array(map(lambda N: trapezoidal(np.exp, 0, 1, N), N))
 trap_error = trap_integral - 1.7182818284590452354
@@ -33,7 +36,6 @@ simp_error = simp_integral - 1.7182818284590452354
 plt.plot(N, simp_error, "b.-", label = "Simpson")
 plt.legend()
 plt.ylabel("Approximation - $\int_0^1 \exp(x)\,dx$")
-
 ax = plt.subplot(2, 1, 2)
 plt.plot(N, trap_error, "g.-", label = "Trapezoidal")
 plt.plot(N, simp_error, "b.-", label = "Simpson")
@@ -44,3 +46,26 @@ plt.ylim(simp_error.min(), simp_error.max())
 ax.yaxis.set_major_formatter(FormatStrFormatter("%0.4f"))
 plt.tight_layout()
 plt.savefig("latex/approximation_errors.pdf")
+
+# evaluates \int_a^b func(x)\,dx using simpson's thing
+# with N, 2N, 4N, 8N, etc subdivisions until requested accuracy is reached
+# (simpson(2^{k+1} N) - simpson(2^k N))/simpson(2^k N) < requested accuracy
+def integrate(func, a, b, N, requested_accuracy):
+	k = 0
+	prev = simpson(func, a, b, N * 2**k)
+	while True:
+		cur = simpson(func, a, b, N * 2**(k+1))
+		if abs((prev - cur)/prev) < requested_accuracy:
+			break
+		k += 1
+		prev = cur
+	return cur
+
+# some tests
+print ("\\int_0^1 e^x\\,dx \\approx %0.10f"
+	% integrate(np.exp, 0, 1, 1, 0.00001))
+print ("\\int_0^1 x^9\\,dx \\approx %0.10f"
+	% integrate(lambda x: x**9, 0, 1, 1, 0.00001))
+
+print scipy.integrate.quad(np.exp, 0, 1)
+print scipy.integrate.romberg(np.exp, 0, 1)

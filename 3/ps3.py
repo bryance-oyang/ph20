3f18901501ce11f38487b96bcf005e177b832f5a
Bryance Oyang <boyang@caltech.edu>
2014-04-25 15:57:18 -0700

    i think i'm done with python code part of ps3

--- 3/ps3.py
+++ 3/ps3.py
@@ -6,9 +6,6 @@ import matplotlib.pyplot as plt
 # do euler method for $\ddot{x} = -x$, with initial conditions
 # $x(0) = 1; \dot{x}(0) = 0$
 def euler(h):
-	# 1   h   xi     x{i+1}
-	#       *      = 
-	# -h  1   vi     v{i+1}
 	#$\begin{pmatrix}1 & h\\-h & 1\end{pmatrix} \begin{pmatrix}x_i\\v_i\end{pmatrix} = \begin{pmatrix}x_{i+1}\\v_{i+1}\end{pmatrix}
 	initial_cond = np.matrix([[1, 0]]).transpose()
 	x = np.array([initial_cond[0, 0]]);
@@ -92,6 +89,7 @@ def euler_implicit(h):
 		v = np.append(v, cur[1, 0])
 	return (x, v)
 
+# max errors vs $h$
 hs = 0.03 * 2.**-np.arange(0,5)
 maxes_imp = np.array([])
 maxes_exp = np.array([])
@@ -131,3 +129,71 @@ plt.ylabel("$E = x^2 + v^2$");
 plt.title("$h = %0.3f$" % (h))
 plt.legend(loc = "upper left");
 plt.savefig("latex/problem5_2.pdf")
+
+# phase space of explicit/implicit
+plt.clf()
+h = 0.1
+t = np.arange(0, int(6*np.pi/h)) * h
+(x, v) = euler(h)
+plt.plot(x, v, "b.-", label = "explicit");
+(x, v) = euler_implicit(h);
+plt.plot(x, v, "g.-", label = "implicit")
+plt.xlabel("$x$")
+plt.ylabel("$v$")
+plt.title("$h = %0.3f$" % (h))
+plt.legend();
+plt.grid(True);
+plt.savefig("latex/p2_problem1.pdf")
+
+def euler_symp(h, end):
+	initial_cond = np.matrix([[1, 0]]).transpose()
+	x = np.array([initial_cond[0, 0]]);
+	v = np.array([initial_cond[1, 0]]);
+	symp = np.matrix([[1, h], [-h, 1 - h**2]])
+	cur = initial_cond;
+	for k in range(1, int(end/h)):
+		cur = symp * cur
+		x = np.append(x, cur[0, 0])
+		v = np.append(v, cur[1, 0])
+	return (x, v)
+
+# phase space of symp
+plt.clf()
+h = 0.1
+t = np.arange(0, int(6*np.pi/h)) * h
+(x, v) = euler_symp(h, 6*np.pi)
+plt.plot(x, v, "r.-");
+plt.xlabel("$x$")
+plt.ylabel("$v$")
+plt.title("$h = %0.3f$" % (h))
+plt.grid(True);
+plt.savefig("latex/p2_problem2.pdf")
+
+# energy as function of $t$
+plt.clf()
+h = 0.1;
+(x, v) = euler_symp(h, 6 * np.pi);
+t = np.arange(0, int(6*np.pi/h)) * h
+E = x**2 + v**2;
+plt.plot(t, E, "r.-")
+plt.xlabel("$t$");
+plt.ylabel("$E = x^2 + v^2$");
+plt.title("$h = %0.3f$" % (h))
+plt.savefig("latex/p2_problem3.pdf")
+
+# long term evolution; plotting last 3 cycles after 360 cycles total
+# to see lag between symp and anal
+plt.clf()
+h = 0.1;
+cycles = 360;
+(x, v) = euler_symp(h, 2 * np.pi * cycles)
+t = np.arange(0, int(2 * np.pi * cycles/h))*h
+x_anal = np.cos(t)
+plt.plot(t, x, "r.-", label = "Sympl")
+plt.plot(t, x_anal, "b.-", label = "Analytic")
+plt.legend()
+plt.xlim([2 * np.pi * (cycles - 3), 2 * np.pi * cycles]);
+plt.xlabel("$t$")
+plt.ylabel("$x(t)$")
+plt.title("$h = %0.3f$" % (h))
+plt.savefig("latex/p2_problem4.pdf")
